bec9b182518c60f3336115a090ef93b475995cd9
Ignacio Fernandez Dussaut <Ignacio.Dussaut@Cartesian.com>
2025-11-11 11:17:32 +0000

    init(package): add initial package structure
    Added initial package files for the Python template project, including the main package and test package. This establishes the foundational structure for future development.
    
    - Created __init__.py for the main package with versioning.
    - Created __init__.py for the test package.
    - Added version file to track the package version.

---
+++ py_template/__init__.py
@@ -0,0 +1,6 @@
+"""Python template package.
+
+This package provides a template structure for Python projects.
+"""
+
+__version__ = "0.1.0"
---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+"""Test package for py-template.
+
+This package contains all the test modules for the py-template project.
+"""
